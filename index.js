96a85232118dc38a0ed36b8ce05a34b028ac89fc
Ivan Storck <ivan@ivanstorck.com>
2023-08-11 14:18:50 +0100

    feat: speed and location

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /* global geolocator */
 function round(number, precision) {
-  var shift = function (num, precis, reverseShift) {
+  const shift = function (num, precis, reverseShift) {
     if (reverseShift) {
       precis = -precis
     }
-    var numArray = ('' + num).split('e')
+    const numArray = ('' + num).split('e')
     return +(
       numArray[0] + 'e' + (numArray[1] ? +numArray[1] + precis : precis)
     )
@@ -12,68 +12,27 @@ function round(number, precision) {
   return shift(Math.round(shift(number, precision, false)), precision, true)
 }
 
+function mstomph(ms) { return ms*2.23694 }
+
 window.onload = function () {
-  var options = {
-    enableHighAccuracy: true,
-    timeout: 10000,
-    maximumWait: 20000,
-    maximumAge: 0
-  }
 
-  // @ts-ignore
-  geolocator.locate(options, function (err, location) {
-    if (err) return geoLocationFail(err, location)
-    $.get(
-      'https://api.weather.gov/points/' +
-        location.coords.latitude +
-        ',' +
-        location.coords.longitude,
-      getFirstStation
-    ).fail(function () {
-      $('h2.answer').text('Sorry, weather service API did not work right now')
-    })
+  navigator.geolocation.getCurrentPosition(function (position) {
+    Window.map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 10);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+    }).addTo(Window.map);
+    setInterval(updater, 5000, location)
+    updater(position)
   })
 }
 
-function geoLocationFail(err, location) {
-  $('h2.answer').text(
-    'Sorry, geolocation did not work, or is not enabled in your browser'
-  )
-  console.error(err.message)
-  console.log(location)
-}
-
-// get the list of weather stations near the user's location
-function getFirstStation(data) {
-  var observationStationsURL = data.properties.observationStations
-  $.get(observationStationsURL, function (stations) {
-    stationData(stations.observationStations[0])
-  }).fail(function () {
-    $('h2.answer').text(
-      'Sorry, the Weather Service API observations stations list was not available'
-    )
+function updater(location) {
+  navigator.geolocation.getCurrentPosition(function(l2){
+    console.log(l2)
+    const speed = round(mstomph(l2.coords.speed), 2)
+    $('h2.answer').text(speed)
+    Window.map.setView([l2.coords.latitude, l2.coords.longitude], 13);
+    // const marker = L.marker([l2.coords.latitude, l2.coords.longitude]).addTo(Window.map);
   })
-}
-
-// get current observation for station
-function stationData(url) {
-  $.get(url, function (station) {
-    $.get(
-      `https://api.weather.gov/stations/${station.properties.stationIdentifier}/observations/`,
-      function (data) {
-        updater(data.features[0].properties.temperature.value)
-        $('span.location').text(station.properties.name)
-      }
-    )
-  })
-}
-
-function updater(tmp) {
-  var tempF = round(tmp * (9 / 5) + 32, 0)
-  var coldEnough = tempF <= 46
-  if (coldEnough) {
-    $('h2.answer').text('🍺 HELL YEAH!')
-  } else {
-    $('h2.answer').text('🌴 No way, José. It\'s only ' + tempF + ' degrees.')
-  }
 }
